36141c3acb7ecdc5cbeb7257e52d9df2ae3c0087
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-21 06:33:18 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,11 +4,13 @@ end
 
 def batch_badge_creator(attendees)
   badges = []
-  for name in attendees
-    badges << badge_maker(name)
-  end
+  attendees.each { |name| badges << badge_maker(name) }
   badges
 end
 
 def assign_rooms(attendees)
+  rooms = []
+  attendees.each_with_index do |name, index|
+    rooms << "Hello, #{name}! You'll be assigned to room #{index + 1}"
+  end
 end
